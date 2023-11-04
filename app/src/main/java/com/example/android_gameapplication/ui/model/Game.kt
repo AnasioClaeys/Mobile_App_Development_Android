b1ca18246271e6472d912a120eefc0403b7d0ce1
AnasioClaeys <100084778+AnasioClaeys@users.noreply.github.com>
2023-11-04 14:37:35 +0100

    Added Mockdata

---
+++ app/src/main/java/com/example/android_gameapplication/ui/model/Game.kt
@@ -0,0 +1,10 @@
+package com.example.android_gameapplication.ui.model
+
+data class Game(
+    val id: Int,
+    val title: String,
+    val brand: String,
+    val genre: String,
+    val year: Int,
+    val platform: String
+)
